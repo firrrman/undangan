45f9d5fd6eef9580fb4cae4ea5e11c8edadc971d
firrrman <firmanhakim11704@gmail.com>
2025-09-07 02:20:46 +0700

    finish

--- components/CoverSection/index.tsx
+++ components/CoverSection/index.tsx
@@ -1,5 +1,5 @@
-import { useEffect } from 'react';
-import { MdArrowDownward } from 'react-icons/md';
+import { useEffect } from "react";
+import { MdArrowDownward } from "react-icons/md";
 
 interface Props {
   handlePlayMusic: () => void;
@@ -9,32 +9,46 @@ interface Props {
 
 const CoverSection = ({ handlePlayMusic, handleOpen, namaTamu }: Props) => {
   const handleOpenInvitation = () => {
-    document.body.classList.remove('overflow-hidden');
+    document.body.classList.remove("overflow-hidden");
     handleOpen();
     handlePlayMusic();
   };
 
   useEffect(() => {
-    document.body.classList.add('overflow-hidden');
+    document.body.classList.add("overflow-hidden");
   }, []);
 
   return (
     <section className="flex justify-center items-center fixed top-0 left-0 w-full h-screen bg-white z-[999]">
       <div className="container mx-auto h-full flex flex-col justify-center items-center">
-        <h3 className="sm:text-[32px] text-[24px] font-medium text-zinc-800 leading-tight mb-4" data-aos="fade-down">
+        <h3
+          className="sm:text-[32px] text-[24px] font-medium text-zinc-800 leading-tight mb-4"
+          data-aos="fade-down"
+        >
           The Wedding of
         </h3>
-        <h1 className="sm:text-8xl text-6xl font-secondary text-zinc-800 leading-tight mb-2" data-aos="fade-down" data-aos-delay="300">
+        <h1
+          className="sm:text-8xl text-6xl font-secondary text-zinc-800 leading-tight mb-2"
+          data-aos="fade-down"
+          data-aos-delay="300"
+        >
           Mahesa & Herla
         </h1>
         <p>Kepada Yth.</p>
         <p>Bapak/Ibu/Sahabat:</p>
         <p className="mb-10 font-semibold capitalize">{namaTamu}</p>
-        <button className="btn btn-primary rounded-md" onClick={handleOpenInvitation} data-aos="fade-down" data-aos-delay="800">
+        <button
+          className="btn btn-primary rounded-md"
+          onClick={handleOpenInvitation}
+          data-aos="fade-down"
+          data-aos-delay="800"
+        >
           <MdArrowDownward />
           <span className="ml-2">Buka Undangan</span>
         </button>
       </div>
+
+      <p className="absolute bottom-10 text-sm opacity-80">By Firman</p>
     </section>
   );
 };
--- components/HeroSection/index.tsx
+++ components/HeroSection/index.tsx
@@ -1,16 +1,30 @@
 const HeroSection = () => {
   return (
-    <section className="h-screen bg-hero bg-cover bg-center bg-no-repeat bg-fixed relative" id="beranda">
+    <section
+      className="h-screen bg-hero bg-cover bg-center bg-no-repeat bg-fixed relative"
+      id="beranda"
+    >
       <div className="overlay"></div>
       <div className="container relative mx-auto h-full flex flex-col justify-center items-center">
-        <h1 className="sm:text-6xl md:text-8xl text-5xl font-secondary text-zinc-50 leading-tight mt-[200px] mb-4" data-aos="fade-down">
+        <h1
+          className="sm:text-6xl md:text-8xl text-5xl font-secondary text-zinc-50 leading-tight mt-[200px] mb-4"
+          data-aos="fade-down"
+        >
           Mahesa & Cella
         </h1>
-        <p className="text-zinc-200 text-center sm:text-[20px] mb-8" data-aos="fade-down" data-aos-delay="300">
+        <p
+          className="text-zinc-200 text-center sm:text-[20px] mb-8"
+          data-aos="fade-down"
+          data-aos-delay="300"
+        >
           Akan Segera Melangsungkan Pernikahan Suci
         </p>
-        <h3 className="sm:text-[32px] text-[24px] font-medium text-zinc-100 leading-tight" data-aos="fade-down" data-aos-delay="800">
-          Jum'at, 19 September 2025
+        <h3
+          className="sm:text-[32px] text-[24px] font-medium text-zinc-100 leading-tight"
+          data-aos="fade-down"
+          data-aos-delay="800"
+        >
+          Jum&#39;at, 19 September 2025
         </h3>
       </div>
     </section>
